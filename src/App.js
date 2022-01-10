38abc24c0adcfae99615b507c3d8b16a19464ade
Arslan <ali.arslan319@gmail.com>
2022-01-09 23:41:50 -0500

    create login and logout handlers to change logged in state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true)
+  }
+
+  const logoutHander = () => {
+    setIsLoggedIn(false)
+  }
+
   return (
     <>
     </>
