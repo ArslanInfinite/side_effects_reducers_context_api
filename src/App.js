e47ae4611fcb3b22cc2fbc506495fb0eff39a176
Arslan <ali.arslan319@gmail.com>
2022-01-09 23:58:55 -0500

    import Login, Home, and MainHeader into App.js, redirect pages based on login status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
+import MainHeader from './components/MainHeader/MainHeader'
+
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
@@ -7,12 +11,17 @@ const App = () => {
     setIsLoggedIn(true)
   }
 
-  const logoutHander = () => {
+  const logoutHandler = () => {
     setIsLoggedIn(false)
   }
 
   return (
     <>
+     <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
     </>
   )
 
