4d14094335ff1d644e91361d9853d603f9766181
Arslan <ali.arslan319@gmail.com>
2022-01-12 23:21:11 -0500

    add clearTimeOut with the argument of the setTimeOut function to cleanup before any excessive information is sent out

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,11 +11,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => {
+    const identifier = setTimeout(() => {
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
       )
     }, 500)
+
+    return () => {
+      clearTimeout(identifier)
+    }
+    
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
