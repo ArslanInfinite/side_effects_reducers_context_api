0c388bb2dca8ead12173cc17283fc3d52139f44f
Arslan <ali.arslan319@gmail.com>
2022-01-18 23:31:02 -0500

    import AuthContext in App.js and wrap it around the return value of that component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
+import AuthContext from './store/AuthContext'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -26,13 +27,13 @@ const App = () => {
   }
 
   return (
-    <>
+    <AuthContext.Provider>
      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
   )
 
 }
