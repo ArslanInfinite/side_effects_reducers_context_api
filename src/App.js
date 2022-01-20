2d80838920f3fdd1575e851e1c60145360864769
Arslan <ali.arslan319@gmail.com>
2022-01-19 23:01:01 -0500

    remove onLogout props across the app to centralize it for later

--- src/App.js
+++ src/App.js
@@ -29,8 +29,9 @@ const App = () => {
   return (
     <AuthContext.Provider value={{
       isLoggedIn: isLoggedIn
+      onLogout: logoutHandler
     }}>
-     <MainHeader onLogout={logoutHandler} />
+     <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
