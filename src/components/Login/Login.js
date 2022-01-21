1fba0b6430527fa350002ca222b51c363cbb04ba
Arslan <ali.arslan319@gmail.com>
2022-01-20 23:48:02 -0500

    add useContext to login and replace props with authContext from useContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,8 @@
-import { useState, useEffect, useReducer } from 'react'
+import { useState, useEffect, useReducer, useContext } from 'react'
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
 import Button from '../UI/Button/Button'
+import AuthContext from '../../store/AuthContext'
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -39,6 +40,8 @@ const Login = (props) => {
     value: '', 
     isValid: null
   })
+
+  const authContext = useContext(AuthContext)
 
   useEffect(() => {
     console.log('EFFECT RUNNING')
@@ -96,7 +99,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value)
+    authContext.onLogin(emailState.value, passwordState.value)
   };
 
   return (
