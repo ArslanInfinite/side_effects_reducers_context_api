e02ee7ec9692bc8b24c2056580f48a20fcc4c36b
Arslan <ali.arslan319@gmail.com>
2022-01-20 23:16:58 -0500

    remove all traces of logged status responsibility from App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
-import AuthContext from './store/AuthContext'
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true)
-  }
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false)
-  }
-
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
-      onLogout: logoutHandler
-    }}>
+    <>
      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </AuthContext.Provider>
+      </>
   )
 
 }
